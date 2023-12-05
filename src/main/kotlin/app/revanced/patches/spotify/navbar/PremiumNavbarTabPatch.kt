872a6633d564f4c1076e5a6d3688f50fa668a986
nawras2002 <89320176+nawras2002@users.noreply.github.com>
2023-12-05 23:58:37 +0300

    Update PremiumNavbarTabPatch.kt

--- src/main/kotlin/app/revanced/patches/spotify/navbar/PremiumNavbarTabPatch.kt
+++ src/main/kotlin/app/revanced/patches/spotify/navbar/PremiumNavbarTabPatch.kt
@@ -1,32 +1,59 @@
-package app.revanced.patches.spotify.navbar
+package app.revanced.patches.all.screenshot.removerestriction.patch
 
-import app.revanced.extensions.exception
-import app.revanced.patcher.data.BytecodeContext
-import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
-import app.revanced.patcher.patch.BytecodePatch
-import app.revanced.patcher.patch.annotation.CompatiblePackage
-import app.revanced.patcher.patch.annotation.Patch
-import app.revanced.patches.spotify.navbar.fingerprints.AddNavBarItemFingerprint
+import app.revanced.patcher.annotation.Description
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patcher.patch.annotations.RequiresIntegrations
+import app.revanced.patcher.util.proxy.mutableTypes.MutableMethod
+import app.revanced.util.patch.*
+import org.jf.dexlib2.iface.ClassDef
+import org.jf.dexlib2.iface.Method
+import org.jf.dexlib2.iface.instruction.Instruction
+import java.util.*
 
-@Patch(
-    name = "Hide premium navbar",
-    description = "Removes the premium tab from the navbar.",
-    dependencies = [PremiumNavbarTabResourcePatch::class],
-    compatiblePackages = [CompatiblePackage("com.spotify.music")]
-)
-@Suppress("unused")
-object PremiumNavbarTabPatch : BytecodePatch(
-    setOf(AddNavBarItemFingerprint)
-) {
-    // If the navigation bar item is the premium tab, do not add it.
-    override fun execute(context: BytecodeContext) = AddNavBarItemFingerprint.result?.mutableMethod?.addInstructions(
-        0,
-        """
-            const v1, ${PremiumNavbarTabResourcePatch.premiumTabId}
-            if-ne p5, v1, :continue
-            return-void
-            :continue
-            nop
-        """
-    ) ?: throw AddNavBarItemFingerprint.exception
+@Patch(false)
+@Name("remove-screenshot-restriction")
+@Description("Removes the restriction of taking screenshots in apps that normally wouldn't allow it.")
+@Version("0.0.1")
+@RequiresIntegrations
+internal class RemoveScreenshotRestrictionPatch : AbstractTransformInstructionsPatch<Instruction35cInfo>() {
+
+    private companion object {
+        const val INTEGRATIONS_CLASS_DESCRIPTOR_PREFIX =
+            "Lapp/revanced/all/screenshot/removerestriction/RemoveScreenshotRestrictionPatch"
+        const val INTEGRATIONS_CLASS_DESCRIPTOR = "$INTEGRATIONS_CLASS_DESCRIPTOR_PREFIX;"
+    }
+
+    // Information about method calls we want to replace
+    enum class MethodCall(
+        override val definedClassName: String,
+        override val methodName: String,
+        override val methodParams: Array<String>,
+        override val returnType: String
+    ): IMethodCall {
+        SetFlags(
+            "Landroid/view/Window;",
+            "setFlags",
+            arrayOf("I", "I"),
+            "V",
+        );
+    }
+
+    override fun filterMap(
+        classDef: ClassDef,
+        method: Method,
+        instruction: Instruction,
+        instructionIndex: Int
+    ) = filterMapInstruction35c<MethodCall>(
+        INTEGRATIONS_CLASS_DESCRIPTOR_PREFIX,
+        classDef,
+        instruction,
+        instructionIndex
+    )
+
+    override fun transform(mutableMethod: MutableMethod, entry: Instruction35cInfo) {
+        val (methodType, instruction, instructionIndex) = entry
+        methodType.replaceInvokeVirtualWithIntegrations(INTEGRATIONS_CLASS_DESCRIPTOR, mutableMethod, instruction, instructionIndex)
+    }
 }
